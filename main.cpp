10bc0176e4ce929a4a06ed4e767eba9fa075aa62
unknown <dylan.castro@utec.edu.pe>
2018-09-22 01:03:08 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <cstdlib>
 #include <iostream>
 #include "tree.h"
 using namespace std;
 int main(int argc, char *argv[]) {
-    Tree<int>* homework= new Tree<int>();
-    homework->Main();
+    Tree<int> homework;
+    homework.Main();
     system("pause");
     return EXIT_SUCCESS;
 }
--- tree.h
+++ tree.h
@@ -1,7 +1,9 @@
 #ifndef TREE_H
 #define TREE_H
+#include <cstdlib>
 #include <iostream>
 #include "node.h"
+#include "iterator.h"
 using namespace std;
 template <typename T>
 class Tree
@@ -16,23 +18,74 @@ class Tree
         void InOrder(Node<T>* root);
         void PostOrder(Node<T>* root);
         void Main();
+        Iterator<T> begin();
 };
 template<class T> Tree<T>::Tree(){
     root=nullptr;
 }
+template<class T> Tree<T>::~Tree(){
+    root=nullptr;
+}
 template<class T> void Tree<T>::Main(){
-    int date[]={8,3,10,1,6,14,4,7,13};
+    int opcion;
+    int date;
+    Node<T>** pointer;
+    do{
+        cout << "Bienvenido al arbol binario" << endl;
+        cout << "1) Insertar dato" << endl;
+        cout << "2) Eliminar dato" << endl;
+        cout << "3) Imprimir en PreOrder" << endl;
+        cout << "4) Imprimir en InOrder" << endl;
+        cout << "5) Imprimir en PostOrder" << endl;
+        cout << "6) Salir" <<endl;
+        cout << "Eliga el numero respectivo a lo que desea hacer:";
+        cin >> opcion;
+        switch(opcion){
+        case 1:
+            cout << "Ingrese un numero: ";
+            cin >> date;
+            pointer=&root;
+            Insert(pointer,date);
+            pointer=nullptr;
+            system("cls");
+            cout << "PreOrder:" <<endl;
+            PreOrder(root);
+            cout << endl;
+            break;
+        case 3:
+            system("cls");
+            cout << "PreOrder:" <<endl;
+            PreOrder(root);
+            cout << endl;
+            break;
+        case 4:
+            system("cls");
+            cout << "InOrder:" <<endl;
+            InOrder(root);
+            cout << endl;
+            break;
+        case 5:
+            system("cls");
+            cout << "PostOrder:" <<endl;
+            PostOrder(root);
+            cout << endl;
+            break;
+        default:
+            system("cls");
+            break;
+        }
+    }
+    while(opcion!=6);
+    /*int date[]={8,3,10,1,6,14,4,7,13};
     for(int x=0;x<(sizeof(date) / sizeof(*date));x++){
-        Node<T>** pointer=&root;
-        Insert(pointer,date[x]);
-        pointer=nullptr;
+
     }
     PreOrder(root);
     cout << endl;
     InOrder(root);
     cout << endl;
     PostOrder(root);
-    cout << endl;
+    cout << endl;*/
 }
 template<class T> void Tree<T>::Insert(Node<T> **&pointer,T data){
     if(*pointer==nullptr){
